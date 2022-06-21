4ff39253965874c14711b43620569f6af18c4e3e
rafaelspa <rsdepaula67@gmail.com>
2022-06-21 14:27:49 -0300

    Commit inicial da minha aplicacao

---
+++ src/main/java/demogradle/App.java
@@ -0,0 +1,13 @@
+package demogradle;
+
+
+public class App {
+    public String getGretting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGretting());
+    }
+}
+
