bdb64fda04f7216e8770e70ab1c2d705156e70a3
rafaelspa <rsdepaula67@gmail.com>
2022-06-22 15:20:11 -0300

    Classe Circulo Test completa

--- src/test/java/demogradle/CirculoTest.java
+++ src/test/java/demogradle/CirculoTest.java
@@ -1,6 +1,7 @@
 package demogradle;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CirculoTest extends TestCase {
@@ -12,15 +13,57 @@ public class CirculoTest extends TestCase {
     }
 
     @Test
-    public void testeDeConstrutorComRaioUm() {
+    public void testeDeConstrutorComRaioPositivo() {
         Circulo circulo = new Circulo(1.0);
         assertTrue(circulo instanceof Circulo);
         assertEquals(1.0, circulo.getRaio());
     }
 
+    @Test()
+    public void testeDeConstrutorComRaioNegativo() {
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Circulo(-1.0));
+    }
+
     @Test
     public void circuloDeRaioUmTemAreaIgualAPI() {
         Circulo circulo = new Circulo(1.0);
+        assertEquals(3.141592653589793, circulo.calcularArea());
+    }
+
+    @Test
+    public void circuloDeDoisEMeioUmTemAreaIgualDezenovePontoSeisTresQuatroEtc() {
+        Circulo circulo = new Circulo(2.5);
+        assertEquals(19.634954085, circulo.calcularArea());
+    }
+
+    @Test
+    public void circuloDeRaioUmTemPerimetro2Pi () {
+        Circulo circulo = new Circulo(1.0);
+        assertEquals(6.283185307, circulo.calcularPerimetro());
+    }
+
+    @Test
+    public void circuloDeRaioDoisEMeioTemPerimetroQuinzePontoSeteZeroSeteNoveEtc () {
+        Circulo circulo = new Circulo(1.0);
+        assertEquals(15.707963268, circulo.calcularPerimetro());
+    }
+
+    @Test
+    public void setaRaioParaUmNumeroPositivo() {
+        Circulo circulo = new Circulo();
+        assertEquals(1.0, circulo.getRaio());
+    }
+
+    @Test
+    public void setaRaioParaNumeroNegativo() {
+        Circulo circulo = new Circulo();
+        Assert.assertThrows(IllegalArgumentException.class, () -> circulo.setRaio(-1.0));
+    }
+
+    @Test
+    public void pegaORaioDeUmCirculoDeRaioUm() {
+        Circulo circulo = new Circulo(1.0);
+        assertEquals(1.0, circulo.getRaio());
     }
 
 }
