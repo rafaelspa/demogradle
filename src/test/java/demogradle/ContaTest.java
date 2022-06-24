6790aa76a50a023ebdf25b24962bef6c527cdca5
rafaelspa <rsdepaula67@gmail.com>
2022-06-24 14:30:12 -0300

    Refatorando os testes de Conta

--- src/test/java/demogradle/ContaTest.java
+++ src/test/java/demogradle/ContaTest.java
@@ -1,167 +1,251 @@
 package demogradle;
 
-import junit.framework.TestCase;
-import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ContaTest extends TestCase {
+import static org.junit.jupiter.api.Assertions.*;
 
-    @Test
-    public void construtorCriaContaSemArgumento() {
-        Conta conta = new Conta();
-        Assert.assertTrue(conta instanceof Conta);
+public class ContaTest{
+
+    Conta conta;
+    Conta contaComCodigoUmELimiteCinco;
+
+    @BeforeEach
+    public void setUp() {
+        conta = new Conta();
+        contaComCodigoUmELimiteCinco = new Conta(1,5.0);
     }
 
     @Test
-    public void construtorCriaContaComCodigoUmELimiteUm() {
-        Conta conta = new Conta(1,1.0);
-        assertTrue( conta instanceof Conta);
-        assertEquals(1, conta.getCodigo());
-        assertEquals(1.0, conta.getLimite());
+    public void construirContaCriaConta() {
+        // Arrange
+        // Act
+        // Assert
+        assertInstanceOf(Conta.class, conta);
+    }
+
+    @Test
+    public void construirCriaContaComCodigoUmELimiteCinco() {
+        // Arrange
+        // Act
+        // Assert
+        assertInstanceOf(Conta.class, contaComCodigoUmELimiteCinco);
+        assertEquals(1, contaComCodigoUmELimiteCinco.getCodigo());
+        assertEquals(5.0, contaComCodigoUmELimiteCinco.getLimite());
     }
 
     @Test
     public void consultarSaldoDeContaRetornaCodigoESaldo() {
-        Conta conta = new Conta(1,1.0);
-        conta.depositar(1.0);
-        assertEquals("\nConta: 1\nSaldo: 1.0", conta.consultarSaldo());
+        // Arrange
+        // Act
+        contaComCodigoUmELimiteCinco.depositar(1.0);
+        // Assert
+        assertEquals("\nConta: 1\nSaldo: 1.0", contaComCodigoUmELimiteCinco.consultarSaldo());
     }
 
     @Test
     public void depositarValorPositivoRetornaTrue() {
-        Conta conta = new Conta();
-        assertTrue(conta.depositar(1.0));
+        // Arrange
+        // Act
+        Boolean depositoComValorPositivo = conta.depositar(1.0);
+        // Assert
+        assertTrue(depositoComValorPositivo);
         assertEquals(1.0, conta.getSaldo());
     }
 
     @Test
     public void depositarZeroRetornaFalse() {
-        Conta conta = new Conta();
-        assertFalse(conta.depositar(0));
+        // Arrange
+        Boolean depositoComValorZero;
+        // Act
+        depositoComValorZero = conta.depositar(0);
+        // Assert
+        assertFalse(depositoComValorZero);
     }
 
     @Test
     public void depositarValorNegativoRetornaFalse() {
-        Conta conta = new Conta();
-        assertFalse(conta.depositar(-1.0));
+        // Arrange
+        Boolean depositoComValorNegativo;
+        // Act
+        depositoComValorNegativo = conta.depositar(-1.0);
+        // Assert
+        assertFalse(depositoComValorNegativo);
     }
 
     @Test
     public void sacarComValorAcimadoSaldoRetornaTrue() {
-        Conta conta = new Conta();
-        conta.depositar(10.0);
-        assertTrue(conta.sacar(5.0));
-        assertEquals(5.0, conta.getSaldo());
+        // Arrange
+        contaComCodigoUmELimiteCinco.depositar(10.0);
+        Boolean saqueMenorQueOSaldo;
+        // Act
+        saqueMenorQueOSaldo = contaComCodigoUmELimiteCinco.sacar(5.0);
+        // Assert
+        assertTrue(saqueMenorQueOSaldo);
+        assertEquals(5.0, contaComCodigoUmELimiteCinco.getSaldo());
     }
 
     @Test
     public void sacarComValorIgualAoSaldoRetornaTrue() {
-        Conta conta = new Conta();
-        conta.depositar(10.0);
-        assertTrue(conta.sacar(10.0));
-        assertEquals(0.0, conta.getSaldo());
+        // Arrange
+        contaComCodigoUmELimiteCinco.depositar(10.0);
+        Boolean saqueIgualAoSaldo;
+        // Act
+        saqueIgualAoSaldo = contaComCodigoUmELimiteCinco.sacar(10.0);
+        // Assert
+        assertTrue(saqueIgualAoSaldo);
+        assertEquals(0.0, contaComCodigoUmELimiteCinco.getSaldo());
     }
 
     @Test
     public void sacarComValorAcimaDoSaldoEMenorQueOLimiteRetornaTrue() {
-        Conta conta = new Conta(1,5.0);
-        conta.depositar(10.0);
-        assertTrue(conta.sacar(12.0));
-        assertEquals(-2.0, conta.getSaldo());
-        assertEquals(3.0, conta.getLimite());
+        // Arrange
+        contaComCodigoUmELimiteCinco.depositar(10.0);
+        Boolean saqueMaiorQueOSaldoEMenorQueOLimite;
+        // Act
+        saqueMaiorQueOSaldoEMenorQueOLimite = contaComCodigoUmELimiteCinco.sacar(12.0);
+        // Assert
+        assertTrue(saqueMaiorQueOSaldoEMenorQueOLimite);
+        assertEquals(-2.0, contaComCodigoUmELimiteCinco.getSaldo());
+        assertEquals(3.0, contaComCodigoUmELimiteCinco.getLimite());
     }
 
     @Test
     public void sacarComValorAbaixoDoSaldoRetornaFalse() {
-        Conta conta = new Conta(1,5.0);
-        conta.depositar(10.0);
-        assertFalse(conta.sacar(16.0));
+        // Arrange
+        contaComCodigoUmELimiteCinco.depositar(10.0);
+        Boolean saqueMaiorQueOLimite;
+        // Act
+        saqueMaiorQueOLimite = contaComCodigoUmELimiteCinco.sacar(16.0);
+        // Assert
+        assertFalse(saqueMaiorQueOLimite);
     }
 
     @Test
     public void transferirComValorAcimaDoSaldoRetornaTrue() {
-        Conta conta1 = new Conta(1,5.0);
-        Conta conta2 = new Conta(1,5.0);
-
-        conta1.depositar(10.0);
-        assertTrue(conta1.transferir(conta2,10.0));
-        assertEquals(0.0, conta1.getSaldo());
-        assertEquals(10.0, conta2.getSaldo());
+        // Arrange
+        contaComCodigoUmELimiteCinco.depositar(10.0);
+        Boolean transferenciaMenorQueOSaldo;
+        // Act
+        transferenciaMenorQueOSaldo = contaComCodigoUmELimiteCinco.transferir(conta, 10.0);
+        // Assert
+        assertTrue(transferenciaMenorQueOSaldo);
+        assertEquals(0.0, contaComCodigoUmELimiteCinco.getSaldo());
+        assertEquals(10.0, conta.getSaldo());
     }
 
     @Test
     public void transferirComValorAcimaDoSaldoEMenorQueOLimiteRetornaTrue() {
-        Conta conta1 = new Conta(1,5.0);
-        Conta conta2 = new Conta(1,5.0);
-
-        conta1.depositar(10.0);
-        assertTrue(conta1.transferir(conta2,12.0));
-        assertEquals(-2.0, conta1.getSaldo());
-        assertEquals(12.0, conta2.getSaldo());
+        // Arrange
+        contaComCodigoUmELimiteCinco.depositar(10.0);
+        Boolean transferenciaMaiorQueSaldoEMenorQueOLimite;
+        // Act
+        transferenciaMaiorQueSaldoEMenorQueOLimite = contaComCodigoUmELimiteCinco.transferir(conta,12.0);
+        // Assert
+        assertTrue(transferenciaMaiorQueSaldoEMenorQueOLimite);
+        assertEquals(-2.0, contaComCodigoUmELimiteCinco.getSaldo());
+        assertEquals(12.0, conta.getSaldo());
     }
 
     @Test
     public void transferirComValorAbaixoDoSaldoRetornaFalse() {
-        Conta conta1 = new Conta(1,5.0);
-        Conta conta2 = new Conta(1,5.0);
-
-        conta1.depositar(10.0);
-        assertFalse(conta1.transferir(conta2,16.0));
+        // Arrange
+        contaComCodigoUmELimiteCinco.depositar(10.0);
+        Boolean transferenciaMaiorQueOLimite;
+        // Act
+        transferenciaMaiorQueOLimite = contaComCodigoUmELimiteCinco.transferir(conta,16.0);
+        // Assert
+        assertFalse(transferenciaMaiorQueOLimite);
     }
 
     @Test
     public void setarCodigoComValorDoisMudaOCodigoParaDois() {
-        Conta conta = new Conta(1,1.0);
-        conta.setCodigo(2);
-        assertEquals(2, conta.getCodigo());
+        // Arrange
+        // Act
+        contaComCodigoUmELimiteCinco.setCodigo(2);
+        // Assert
+        assertEquals(2, contaComCodigoUmELimiteCinco.getCodigo());
     }
 
     @Test
-    public void setarCodigoComValorAbaixoOuIgualAZeroRetornaIllegalArgumentException() {
-        Conta conta = new Conta(1,1.0);
-        Assert.assertThrows(IllegalArgumentException.class, () -> {
-            conta.setCodigo(0);
-        });
+    public void setarCodigoComValorZeroRetornaIllegalArgumentException() {
+        // Arrange
+        // Act
+        // Assert
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    conta.setCodigo(0);
+                },
+                "O código da conta deve ser maior que zero."
+                );
     }
 
     @Test
-    public void getCodigoUmRetornaCodigoUm() {
-        Conta conta = new Conta(1, 1.0);
-        assertEquals(1, conta.getCodigo());
+    public void setarCodigoComValorNegativoRetornaIllegalArgumentException() {
+        // Arrange
+        // Act
+        // Assert
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    conta.setCodigo(-1);
+                },
+                "O código da conta deve ser maior que zero."
+        );
+    }
+
+    @Test
+    public void pegarCodigoUmRetornaCodigoUm() {
+        // Arrange
+        // Act
+        // Assert
+        assertEquals(1, contaComCodigoUmELimiteCinco.getCodigo());
     }
 
     @Test
     public void setarLimiteComValorDoisMudaOLimiteParaDois() {
-        Conta conta = new Conta(1,5.0);
-        conta.setLimite(2.0);
-        assertEquals(2.0, conta.getLimite());
+        // Arrange
+        // Act
+        contaComCodigoUmELimiteCinco.setLimite(2.0);
+        // Assert
+        assertEquals(2.0, contaComCodigoUmELimiteCinco.getLimite());
     }
 
     @Test
-    public void setarLimiteComValorIgualOuAbaixoDeZeroRetornaIllegalArgumentException() {
-        Conta conta = new Conta(1, 5.0);
-        Assert.assertThrows(
-                "O limite da conta deve ser maior que zero.",
-                IllegalArgumentException.class,
+    public void setarLimiteComValorZeroRetornaIllegalArgumentException() {
+        // Arrange
+        // Act
+        // Assert
+
+        assertThrows(IllegalArgumentException.class,
                 () -> {
-                    conta.setLimite(0);
-                });
+                    contaComCodigoUmELimiteCinco.setLimite(0);
+                },  "O limite da conta deve ser maior que zero.");
+    }
+    @Test
+    public void setarLimiteComValorNegativoRetornaIllegalArgumentException() {
+        // Arrange
+        // Act
+        // Assert
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    contaComCodigoUmELimiteCinco.setLimite(-1.0);
+                },  "O limite da conta deve ser maior que zero.");
     }
 
     @Test
     public void pegarLimiteIgualACincoRetornaCinco() {
-        Conta conta = new Conta(1, 5.0);
-        assertEquals(5.0, conta.getLimite());
+        // Arrange
+        // Act
+        // Assert
+        assertEquals(5.0, contaComCodigoUmELimiteCinco.getLimite());
     }
 
     @Test
     public void pegarSaldoIgualADezRetornaDez() {
-        Conta conta = new Conta(1,5.0);
+        // Arrange
         conta.depositar(10.0);
+        // Act
+        // Assert
         assertEquals(10.0, conta.getSaldo());
     }
-
-
-
-
 }
