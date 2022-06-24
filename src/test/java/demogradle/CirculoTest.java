7297dc6aadeac3fa9cf1df149e05d89c96676b1c
rafaelspa <rsdepaula67@gmail.com>
2022-06-24 14:45:13 -0300

    Refatorando os testes de CirculoTest

--- src/test/java/demogradle/CirculoTest.java
+++ src/test/java/demogradle/CirculoTest.java
@@ -1,71 +1,113 @@
 package demogradle;
 
-import junit.framework.TestCase;
-import org.junit.Assert;
-//import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class CirculoTest extends TestCase {
+import static org.junit.jupiter.api.Assertions.*;
 
-    @Test
-    public void testeDeConstrutorSemArgumento() {
-        Circulo circulo = new Circulo();
-        assertTrue(circulo instanceof Circulo);
+public class CirculoTest {
+
+    Circulo circulo;
+    Circulo circuloDeRaioUm;
+    Circulo circuloDeRaioDoisEMeio;
+
+    @BeforeEach
+    public void setUp() {
+        circulo = new Circulo();
+        circuloDeRaioUm = new Circulo(1.0);
+        circuloDeRaioUm = new Circulo(2.5);
     }
 
     @Test
-    public void testeDeConstrutorComRaioPositivo() {
-        Circulo circulo = new Circulo(1.0);
-        assertTrue(circulo instanceof Circulo);
-        assertEquals(1.0, circulo.getRaio());
+    public void construirCirculoRetornaCirculo() {
+        // Arrange
+        // Act
+        // Assert
+        assertInstanceOf(Circulo.class, circulo);
     }
 
     @Test
-    public void testeDeConstrutorComRaioNegativo() {
-        Assert.assertThrows(IllegalArgumentException.class, () -> new Circulo(-1.0));
+    public void construirCirculoComRaioUmRetornaCirculoDeRaioUm() {
+        // Arrange
+        // Act
+        // Assert
+        assertInstanceOf(Circulo.class, circuloDeRaioUm);
+        assertEquals(1.0, circuloDeRaioUm.getRaio());
     }
 
     @Test
-    public void circuloDeRaioUmTemAreaIgualAPi() {
-        Circulo circulo = new Circulo(1.0);
-        assertEquals(3.141592653589793, circulo.calcularArea(), 0.0001);
+    public void construirCirculoComRaioNegativoRetornaIllegalArgumentException() {
+        // Arrange
+        // Act
+        // Assert
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    Circulo circuloDeRaioNegativo = new Circulo(-1.0);
+                },
+                "O raio do círculo deve ser maior que zero."
+        );
     }
 
     @Test
-    public void circuloDeDoisEMeioUmTemAreaIgualDezenovePontoSeisTresQuatroEtc() {
-        Circulo circulo = new Circulo(2.5);
-        assertEquals(19.634954085, circulo.calcularArea(),0.0000001);
+    public void calcularAreaDeCirculoDeRaioUmRetornaAreaIgualAPi() {
+        // Arrange
+        // Act
+        // Assert
+        assertEquals(3.141592653589793, circuloDeRaioUm.calcularArea(), 0.0001);
     }
 
     @Test
-    public void circuloDeRaioUmTemPerimetro2Pi () {
-        Circulo circulo = new Circulo(1.0);
-        assertEquals(6.283185307, circulo.calcularPerimetro(), 0.0000001);
+    public void calcularAreaDeCirculoDeDoisEMeioRetornaAreaDezenovePontoSeisTresQuatroEtc() {
+        // Arrange
+        // Act
+        // Assert
+        assertEquals(19.634954085, circuloDeRaioDoisEMeio.calcularArea(),0.0000001);
     }
 
     @Test
-    public void circuloDeRaioDoisEMeioTemPerimetroQuinzePontoSeteZeroSeteNoveEtc () {
-        Circulo circulo = new Circulo(2.5);
-        assertEquals(15.707963268, circulo.calcularPerimetro(), 0.0000001);
+    public void calcularPerimetroDeUmCirculoDeRaioUmRetornaPerimetro2Pi () {
+        // Arrange
+        // Act
+        // Assert
+        assertEquals(6.283185307, circuloDeRaioUm.calcularPerimetro(), 0.0000001);
     }
 
     @Test
-    public void setaRaioParaUmNumeroPositivo() {
-        Circulo circulo = new Circulo();
+    public void calcularPerimetroDeUmCirculoDeRaioDoisEMeioTemPerimetroQuinzePontoSeteZeroSeteNoveEtc () {
+        // Arrange
+        // Act
+        // Assert
+        assertEquals(15.707963268, circuloDeRaioDoisEMeio.calcularPerimetro(), 0.0000001);
+    }
+
+    @Test
+    public void setarRaioComUmNumeroPositivoRetornaCirculoComRaioIgualAoValor() {
+        // Arrange
+        // Act
         circulo.setRaio(1.0);
+        // Assert
         assertEquals(1.0, circulo.getRaio());
     }
 
     @Test
-    public void setaRaioParaNumeroNegativo() {
-        Circulo circulo = new Circulo();
-        Assert.assertThrows(IllegalArgumentException.class, () -> circulo.setRaio(-1.0));
+    public void setarRaioComNumeroNegativoRetornaIllegalArgumentException() {
+        // Arrange
+        // Act
+        // Assert
+        assertThrows(IllegalArgumentException.class,
+        () -> {
+            circulo.setRaio(-1.0);
+        },
+            "O raio do círculo deve ser maior que zero."
+        );
     }
 
     @Test
-    public void pegaORaioDeUmCirculoDeRaioUm() {
-        Circulo circulo = new Circulo(1.0);
-        assertEquals(1.0, circulo.getRaio());
+    public void pegarORaioDeUmCirculoDeRaioUmRetornaOValorDoRaio() {
+        // Arrange
+        // Act
+        // Assert
+        assertEquals(1.0, circuloDeRaioUm.getRaio());
     }
 
 }
